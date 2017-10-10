9ae15cdd0f926269ea707b25c3f22590a8c27804
Andy Uzick <andy@uzick.com>
2017-10-10 10:16:21 -0500

    Add project files.

---
+++ RenderingMarker/CommentControl.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using Sitecore.Web.UI;
+
+namespace RenderingMarker
+{
+    public class CommentControl :WebControl
+    {
+        public string Message { get; set; }
+
+        public CommentControl()
+        {
+        }
+
+        public CommentControl(string message)
+        {
+            this.Message = message;
+        }
+
+        protected override void DoRender(HtmlTextWriter output)
+        {
+            output.Write($"<!-- {System.Web.HttpUtility.HtmlEncode(Message)} -->");
+        }
+    }
+}
---
+++ RenderingMarker/Pipelines/InsertRenderings/AddComments.cs
@@ -0,0 +1,25 @@
+﻿using Sitecore.Diagnostics;
+using Sitecore.Layouts;
+using Sitecore.Pipelines.InsertRenderings;
+
+namespace RenderingMarker.Pipelines.InsertRenderings
+{
+    public class AddComments : InsertRenderingsProcessor
+    {
+        public override void Process(InsertRenderingsArgs args)
+        {
+            Assert.ArgumentNotNull((object)args, "args");
+            if (!args.HasRenderings || args.ContextItem == null)
+                return;
+
+            for (var i = args.Renderings.Count - 1; i >= 0; i--)
+            {
+                var name = args.Renderings[i]?.RenderingItem?.DisplayName;
+                var placeholder = args.Renderings[i]?.Placeholder;
+                args.Renderings.Insert(i+1, new RenderingReference(new CommentControl($"End rendering: {name ?? "unknown"}")) { Placeholder = placeholder });
+                args.Renderings.Insert(i, new RenderingReference(new CommentControl($"Begin rendering: {name ?? "unknown"}")) { Placeholder = placeholder });
+            }
+
+        }
+    }
+}
